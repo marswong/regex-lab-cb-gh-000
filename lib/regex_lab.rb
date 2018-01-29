ff73a4110ff7d74990074aef95eb6052c0e27ac6
Mars Wong <marswong618@gmail.com>
2018-01-29 13:28:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  /A-Z{5}/.match(text)
+  /\w{5}/.match(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  /\d{10}/.match?(phone)
 end
